6d5817fa0f3d00c2de1734ac8683f72b1fa11b96
Peter Duerr <duerrp@gmail.com>
2016-06-11 22:10:01 +0900

    Refactor basic structure
    * Replace fake imports with simple, dill-based pickling of all the
      necessary modules

--- pycalc/__init__.py
+++ pycalc/__init__.py
@@ -2,46 +2,73 @@
 
 Written by Peter Duerr
 """
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 
-# Plotting
-import apipkg
+import os
+import dill
+import __main__ as _main_module
+import six
 
-PACKAGES = {
-    'sp': "sympy",
-    'u': "pycalc.units:sympy.physics.units",
-    'np': "numpy",
-    'sc': "scipy",
-    'pd': "pandas",
-    'umath': 'uncertainties.umath',
-}
-"""Top level packages
-"""
+if six.PY2:
+    PYCALC_SESSION_NAME = os.path.expanduser("~/.pycalc_session")
+elif six.PY3:
+    PYCALC_SESSION_NAME = os.path.expanduser("~/.pycalc3_session")
+else:
+    print("Python version not supported")
 
-PLOTTING_PACKAGES = {
-    'plt': 'pycalc.plotting:PLT',
-    'sns': 'pycalc.plotting:SNS',
-}
-"""Plotting modules with auto-setup
-"""
+try:
+    dill.load_session(PYCALC_SESSION_NAME)
+except IOError:
+    print("Re-loading pycalc modules...")
+    import numpy as np
+    _main_module.np = np
 
-CLASSES = {
-    'ufloat': 'uncertainties:ufloat',
-    'Counter': 'collections:Counter',
-}
-"""Additional classes to import
-"""
+    import scipy as sc
+    _main_module.sc = sc
 
-SYMPY_VAR_NAMES = ['x', 'y', 'z', 'alpha', 'beta', 'gamma']
-SYMPY_VARS = {name: "sympy.abc:%s" % name
-              for name in SYMPY_VAR_NAMES}
-"""Variables imported from sympy
-"""
+    import sympy as sp
+    import sympy.physics
+    _main_module.sp = sp
+    _main_module.u = sp.physics.units
+    _main_module.u.B = sympy.physics.units.Unit('byte', 'B')
+    _main_module.u.b = sympy.Rational(1, 8) * sympy.physics.units.B
+    _main_module.u.KiB = 1024 * sympy.physics.units.B
+    _main_module.u.MiB = 1024**2 * sympy.physics.units.B
 
-ALL_IMPORTS = {key: value for dic in [PACKAGES,
-                                      CLASSES,
-                                      SYMPY_VARS,
-                                      PLOTTING_PACKAGES]
-               for key, value in dic.items()}
+    import pandas as pd
+    _main_module.pd = pd
 
-# Lazily import the packages and attributes
-apipkg.initpkg(__name__, ALL_IMPORTS)
+    from matplotlib import pyplot as plt
+    _main_module.plt = plt
+
+    import seaborn as sns
+    _main_module.sns = sns
+
+    dill.dump_session(PYCALC_SESSION_NAME, main=_main_module)
+
+
+def setup_plotting():
+    """Better defaults for plotting
+    """
+    import matplotlib
+    import seaborn as sns
+
+    font = {'family': 'normal',
+            'weight': 'normal',
+            'size': 14}
+
+    matplotlib.rc('font', **font)
+
+    matplotlib.rc('text', usetex=True)
+    matplotlib.rc('lines', linewidth=4)
+    matplotlib.rc('figure', facecolor='white')
+
+    label_size = 14
+    matplotlib.rc('xtick', labelsize=label_size)
+    matplotlib.rc('ytick', labelsize=label_size)
+
+    sns.set_style('darkgrid')
+    sns.set_palette('colorblind',
+                    desat=0.6)
